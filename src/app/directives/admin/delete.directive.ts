4a728f9c4ecd447e0aa3a0842c9ce7a227fc2b51
smlozaydin <samil.bilal.ozaydin@gmail.com>
2023-11-09 18:09:57 +0300

    Deletion directive is completed

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -3,6 +3,7 @@ import { MatDialog } from '@angular/material/dialog';
 import { NgxSpinnerService } from 'ngx-spinner';
 import { SpinnerType } from 'src/app/base/base.component';
 import { DeleteDialogComponent, DeleteState } from 'src/app/dialogs/delete-dialog/delete-dialog.component';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
 import { HttpClientService } from 'src/app/services/common/http-client.service';
 import { ProductService } from 'src/app/services/common/models/product.service';
 
@@ -18,7 +19,8 @@ export class DeleteDirective {
     private _renderer : Renderer2,
     private httpClientService: HttpClientService,
     private spinnerService: NgxSpinnerService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private alertifyService: AlertifyService
   ) {
     const img =_renderer.createElement("img");
     img.setAttribute("src","../../../../../assets/delete.png");
@@ -29,20 +31,36 @@ export class DeleteDirective {
    }
 
    @Output() callback: EventEmitter<any> = new EventEmitter();
-   @Input() id : string
+   @Input() id : string;
+   @Input() controller : string;
    @HostListener("click")
    async onClick(){
     this.openDialog(async ()=>{
       this.spinnerService.show(SpinnerType.BallPulseSync);
       const td : HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).animate({
-        opacity:0,
-        left: "+50",
-        height:"toogle"
-      },1000,()=>{
-        this.callback.emit();
-      })
+      this.httpClientService.delete({controller: this.controller},this.id)
+        .subscribe(data => {
+          $(td.parentElement).animate({
+            opacity:0,
+            left: "+50",
+            height:"toogle"
+          },1000,()=>{
+            this.callback.emit();
+            this.alertifyService.message("Deletion is successfull",{
+                messageType:MessageType.Success,
+                position:Position.BottomRight,
+                dismissOthers:false
+              });
+          })
+        }, (error) =>{
+          this.alertifyService.message("Deletion is not successful. An error is occured",{
+            messageType:MessageType.Error,
+            position:Position.BottomRight,
+            dismissOthers:false
+          });
+          this.spinnerService.hide(SpinnerType.BallPulseSync);
+        });
+
     });
 
    }
